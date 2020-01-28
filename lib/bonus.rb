21d27741d7bb239f53c6410de3243fd06e9479ab
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-28 19:55:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,9 +21,8 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy[:montague][:patriarch][:name] = "dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
- puts epic_tragedy
+ epic_tragedy
 end
