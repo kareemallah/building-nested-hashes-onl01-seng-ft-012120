bbe7db78fa8360a7e48b421c5269b68534dd12e1
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-28 19:49:52 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,10 +22,8 @@ def bonus
 
   #code your solution here:
 epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
- 
-puts epic_tragedy
-  
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+ puts epic_tragedy
 end
