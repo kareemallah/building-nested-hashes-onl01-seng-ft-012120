75aa2118bf9a66e4e2f5e1f24595d4b22f661d74
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-28 19:40:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,14 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-      :patriarch 
-      :matriarch
-      :hero
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => []
      },
-     :capulet => {}
+     :capulet => {
+       :patriarch
+       :matriarch
+     }
    }
 end
